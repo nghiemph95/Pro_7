3537495d6fd7f416ed23b9ef1d019bad4b79b597
nghiemph95 <nghiemph95@gmail.com>
2021-12-18 22:30:35 +0700

    update1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
-
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
-    return (
-        <div>
-            <Announcement/>
-            <Navbar/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
